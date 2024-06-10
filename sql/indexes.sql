fb8cd63908f10947a7730da8486421186a5a1947
Cristian Piero Alioto <cristianpieroalioto@gmail.com>
2024-06-10 21:45:02 +0200

    indici btree tolti

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,17 +1,3 @@
-
-/*Indice per opere interne */
-CREATE INDEX idx_operaI_TitoloB ON operainterna (Titolo);
-
-/*Indice per opere esterne */
-CREATE INDEX idx_operaE_TitoloB ON operaesterna (Titolo);
-
-/*Indice per biglietto*/
-CREATE INDEX idx_Biglietto_NumeroSerialeB ON Biglietto  (NumeroSeriale);
-
-/*Indice per visitatori*/
-CREATE INDEX idx_Visitatori_emailB ON Visitatore (Email)
-
-
 /*Indice per opere interne */
 CREATE INDEX idx_operaI_TitoloH ON operainterna USING HASH (Titolo);
 
