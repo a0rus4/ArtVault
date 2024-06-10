96b01b14366d5d0b1f2f9429545fdfecad3570c5
a0rus4 <112568815+a0rus4@users.noreply.github.com>
2024-06-10 18:25:19 +0200

    Update indexes.sql

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,12 +1,25 @@
 
 /*Indice per opere interne */
-CREATE INDEX idx_operaI_Titolo ON operainterna USING HASH (Titolo);
+CREATE INDEX idx_operaI_TitoloB ON operainterna (Titolo);
 
 /*Indice per opere esterne */
-CREATE INDEX idx_operaE_Titolo ON operaesterna USING HASH (Titolo);
+CREATE INDEX idx_operaE_TitoloB ON operaesterna (Titolo);
 
 /*Indice per biglietto*/
-CREATE INDEX idx_Biglietti_NumeroSeriale ON Direttore USING HASH (NumeroSeriale);
+CREATE INDEX idx_Biglietti_NumeroSerialeB ON Direttore  (NumeroSeriale);
 
 /*Indice per visitatori*/
-CREATE INDEX idx_Visitatori_email ON Visitatore USING HASH (Email)
+CREATE INDEX idx_Visitatori_emailB ON Visitatore (Email)
+
+
+/*Indice per opere interne */
+CREATE INDEX idx_operaI_TitoloH ON operainterna USING HASH (Titolo);
+
+/*Indice per opere esterne */
+CREATE INDEX idx_operaE_TitoloH ON operaesterna USING HASH (Titolo);
+
+/*Indice per biglietto*/
+CREATE INDEX idx_Biglietti_NumeroSerialeH ON Direttore USING HASH (NumeroSeriale);
+
+/*Indice per visitatori*/
+CREATE INDEX idx_Visitatori_emailH ON Visitatore USING HASH (Email)
